b66d9a0f753e7b54504a09fb2cde505815968ce4
Dexametasona <machicadogomezalexander@gmail.com>
2024-08-22 22:19:28 -0500

    feat: initializing project

---
+++ retrueque/src/main/java/com/nocountry/retrueque/RetruequeApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.retrueque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetruequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetruequeApplication.class, args);
+	}
+
+}
