0d5faac78a3d9a29601c929e2fc026263cd3d03c
Dexametasona <machicadogomezalexander@gmail.com>
2024-09-06 15:38:39 -0500

    fix(cors): fixing cors policies @bean annotation un cors configurer

--- retrueque/src/main/java/com/nocountry/retrueque/config/CorsConfig.java
+++ retrueque/src/main/java/com/nocountry/retrueque/config/CorsConfig.java
@@ -1,20 +1,23 @@
 package com.nocountry.retrueque.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-  public WebMvcConfigurer corsConfig(){
+
+  @Bean
+  public WebMvcConfigurer corsCustomConfig(){
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
       }
     };
   }
