1aa6e9c3d25557fb393e5182b75a08d7d8a4ebc8
Dexametasona <machicadogomezalexander@gmail.com>
2024-09-06 16:14:56 -0500

    fix(cors): adding url for cors allow

--- retrueque/src/main/java/com/nocountry/retrueque/config/CorsConfig.java
+++ retrueque/src/main/java/com/nocountry/retrueque/config/CorsConfig.java
@@ -14,7 +14,14 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "http://localhost:3002",
+                        "http://localhost:3003",
+                        "http://localhost:3004",
+                        "http://localhost:3005"
+                        )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
