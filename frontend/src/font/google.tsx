b23da712471cf820ed5c80523019695f8b0ac193
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-09-08 00:35:17 -0300

    add:404

--- frontend/src/font/google.tsx
+++ frontend/src/font/google.tsx
@@ -1,7 +1,22 @@
-import {Lato} from 'next/font/google'
+import {Lato, Rubik_Mono_One, Rubik_Puddles } from 'next/font/google'
 
+//Rubik One
+export const rubikOne = Rubik_Mono_One ({
+    subsets: ['latin', 'latin-ext'],
+    weight: ['400'],
+    variable: '--font-rubik',
+})
+//Lato
 export const lato = Lato ({
     subsets: ['latin', 'latin-ext'],
     weight: ['100','300','400','700','900'],
     variable: '--font-lato',
+})
+
+// Rubik Puddles
+
+export const rubikPuddles = Rubik_Puddles ({
+    subsets: ['latin', 'latin-ext'],
+    weight: ['400'],
+    variable: '--font-rubik-puddles',
 })
