8de5299725898aecac5b4d91292dd41712282e12
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-09-14 11:19:03 -0300

    add:Query Category // connection Home filter/search , test build

--- frontend/src/app/public/search/page.tsx
+++ frontend/src/app/public/search/page.tsx
@@ -1,11 +1,28 @@
+'use client'
 import Filters from '@/components/filters/filters';
 import TopbarGeneral from '@/components/containers/topbar-general';
+import {useState, useEffect, Suspense} from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 
-export default function Resultados() {
+export default function Search() {
+    const [verifyStatus, setVerifyStatus] = useState<string | null>(null);
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    //Obtener los valores
+    const provincia = searchParams.get('provincia') || '';
+    const departamento = searchParams.get('departamento') || '';
+    const category = searchParams.get('category') || '';
+
     return (
         <><TopbarGeneral />
             <main className='w-full max-w-[1232px] mx-auto mt-10'>
-                <Filters className='m-auto' />
+            <Filters
+                    provincia={provincia}
+                    departamento={departamento}
+                    category={category}
+                    className='m-auto'
+                />
             </main>
         </>
     );
--- frontend/src/components/filters/form-home.tsx
+++ frontend/src/components/filters/form-home.tsx
@@ -12,118 +12,178 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
 } from "@/components/ui/form";
-import { provinciasConDepartamentos } from '@/lib/data/provincias';
-import { servicios } from "@/lib/data/servicios";
-import { categorias } from "@/lib/data/categorias";
 import { Button } from "../ui/button";
 import { z } from "zod"
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import useProvincias from "@/hooks/useProvincias"; // Hook de provincias
+import useCategorys from '@/hooks/useCategorys'; // Hook de categorías
+import useDepartaments from '@/hooks/useDepartaments'; // Hook de departamentos
+import { useRouter } from 'next/navigation'
+import { useState, useEffect } from "react";
 
 const formSchema = z.object({
-  provincia: z.string().length(50),
-  departamento: z.string().length(50),
-  categoria: z.string().length(50),
+  provincia: z.string({
+    required_error: "La provincia es obligatoria",
+  }),
+  departamento: z.string({
+    required_error: "Seleccione un departamento",
+  }),
+  category: z.string({
+    required_error: "Seleccione una categoría",
+  }),
 })
 
 function FormHome() {
+  const router = useRouter();
+  const [selectedServicio, setSelectedServicio] = useState<string>("");
+  const [selectedProvinciaId, setSelectedProvinciaId] = useState<number | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const { data: provincias, isLoading: isLoadingProvincias } = useProvincias();
+  const { data: categorys, isLoading: isLoadingCategorys } = useCategorys();
+  const { data: departamentos, isLoading: isLoadingDepartamentos } = useDepartaments(selectedProvinciaId || 0);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues:{
+    defaultValues: {
       provincia: '',
       departamento: '',
-      categoria: '',
+      category: '',
     }
-  })
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    const searchParams = new URLSearchParams({
+      provincia: data.provincia || '',
+      departamento: data.departamento || '',
+      category: data.category || ''
+    });
+
+    router.push(`/public/search?${searchParams.toString()}`);
+  };
+
+  useEffect(() => {
+    // Verificar si provincias está definido antes de usarlo
+    if (provincias) {
+        const subscription = form.watch((data) => {
+            const selectedProvincia = provincias.find(p => p.name === data.provincia);
+            if (selectedProvincia) {
+                setSelectedProvinciaId(selectedProvincia.id);
+            } else {
+                setSelectedProvinciaId(null);
+            }
+        });
+
+        return () => subscription.unsubscribe();
+    }
+}, [provincias, form]);
+
   return (
     <div className="relative flex flex-row items-center justify-center p-10 bg-secondary rounded-lg container w-[65%] gap-4">
       <Form {...form}>
-        <FormField
-          name="provincia"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem className="w-[200px] shadow-md shadow-gray-600 rounded-lg">
-              <FormControl>
-                <Select onValueChange={field.onChange}>
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Provincia" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {provinciasConDepartamentos.map((prov,index) => (
-                      <SelectItem
-                        key={index}
-                        value={prov.provincia}
-                      >
-                        {prov.provincia}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </FormControl>
-            </FormItem>
-          )}
-        />
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex flex-wrap justify-center gap-4 p-4">
 
-        <FormField
-          name="departamento"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem className="w-[200px] shadow-md shadow-gray-600 rounded-lg">
-              <FormControl>
-                <Select onValueChange={field.onChange}>
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Dpto" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {form.watch("provincia") &&
-                      provinciasConDepartamentos
-                        .find(
-                          (prov) =>
-                            prov.provincia === form.watch("provincia")
-                        )
-                        ?.departamentos.map((depto) => (
-                          <SelectItem key={depto} value={depto}>
-                            {depto}
+          {/* Provincia */}
+          <FormField
+            name="provincia"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="w-[150px] shadow-md shadow-gray-600 rounded-lg">
+                <FormControl>
+                  <Select onValueChange={(value) => { field.onChange(value); }} value={field.value || ""}>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Provincia" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {Array.isArray(provincias) ? (
+                        provincias.map((provincia) => (
+                          <SelectItem key={provincia.id} value={provincia.name}>
+                            {provincia.name}
                           </SelectItem>
-                        ))}
-                  </SelectContent>
-                </Select>
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="categoria"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem className="w-[200px] shadow-md shadow-gray-600 rounded-lg">
-              <FormControl>
-                <Select onValueChange={field.onChange}>
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Categoría" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {categorias.map((categoria) => (
-                      <SelectItem key={categoria} value={categoria}>
-                        {categoria}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </FormControl>
-            </FormItem>
-          )}
-        />
+                        ))
+                      ) : (
+                        <SelectItem value="error" disabled>
+                          Cargando
+                        </SelectItem>
+                      )}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-        <Button
-          type="submit"
-          variant="default"
-          className="text-black text-base font-bold leading-normal tracking-tight bg-primary shadow-md shadow-gray-600 rounded-lg px-14"
-        >
-          Buscar
-        </Button>
+          {/* Departamento */}
+          <FormField
+            name="departamento"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="w-[150px] shadow-md shadow-gray-600 rounded-lg">
+                <FormControl>
+                  <Select onValueChange={field.onChange}>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Dpto" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {Array.isArray(departamentos) ? (
+                        departamentos.map((departamento) => (
+                          <SelectItem key={departamento.id} value={departamento.name}>
+                            {departamento.name}
+                          </SelectItem>
+                        ))
+                      ) : (
+                        <SelectItem value="error" disabled>
+                          Cargando
+                        </SelectItem>
+                      )}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          {/* Categoría */}
+          <FormField
+            name="category"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="w-[150px] shadow-md shadow-gray-600 rounded-lg">
+                <FormControl>
+                  <Select onValueChange={field.onChange}>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Categoría" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {Array.isArray(categorys) ? (
+                        categorys.map((category) => (
+                          <SelectItem key={category.id} value={category.name}>
+                            {category.name}
+                          </SelectItem>
+                        ))
+                      ) : (
+                        <SelectItem value="error" disabled>
+                          Cargando
+                        </SelectItem>
+                      )}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          {/* Botón de Buscar */}
+          <Button
+            type="submit"
+            variant="default"
+            className="text-black text-base font-bold leading-normal tracking-tight bg-primary shadow-md shadow-gray-600 rounded-lg px-14"
+          >
+            Buscar
+          </Button>
+        </form>
       </Form>
     </div>
   );
